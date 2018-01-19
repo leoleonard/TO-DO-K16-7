a60022c71d21e94b48b2ecc8a5ae1817a1f09d7e
Damian Juzyk <damian.juzyk@gmail.com>
2018-01-19 14:20:16 +0400

    add basic code

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index.bundle.js'
+    },
+    module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: "babel-loader"
+                }
+            ]
+        }
+    };
